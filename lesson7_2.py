10f37fea5c5bc654bfbd2f1b3bb49b047a791b87
dearkelly <147892143+dearkelly@users.noreply.github.com>
2023-11-04 05:57:05 +0000

    modify

--- lesson7_2.py
+++ lesson7_2.py
@@ -1,33 +1,9 @@
-import random
-def playGame():
-    min = 1
-    max = 100
-    count = 0
-    target = random.randint(min,max)
-    print(target)
-    print("============猜數字遊戲=================\n")
-    while(True):
-        keyin = int(input(f"猜數字的範圍{min}~{max}"))
-        count += 1
-        if(keyin >= min and keyin <= max):
-         if keyin == target:
-             print(f"賓果!猜對了, 答案是:{keyin}")
-             break
-            elif keyin > target:
-                print("再小一點")
-                max = keyin - 1
-            elif keyin < target:
-                print("再大一點")
-             min = keyin + 1
-            print(f"加油@您已經猜了{count}次")
-            continue
-        else:
-            print("請輸入提示範圍內的數字")
-    print(f"您猜了{count}次")
+
+import tools
 
 def main():
     while(True):
-        playGame()
+        tools.playGame()
         playAgain = input("您還要繼續嗎(y,n)?")
         if playAgain == 'n':
             break
