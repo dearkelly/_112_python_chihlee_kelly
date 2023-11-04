34ed9b12ad996aa1d0689f2b0a0944e454acc937
dearkelly <147892143+dearkelly@users.noreply.github.com>
2023-11-04 06:18:48 +0000

    modify

---
+++ lesson8_1.py
@@ -0,0 +1,8 @@
+from tools import getStudent
+
+stu1 = getStudent(name="robert",chinese=89,english=92,math=19)
+print(stu1.name)
+print(stu1.chinese)
+print(stu1.english)
+print(stu1.math)
+print(stu1.sum())
--- tools.py
+++ tools.py
@@ -25,3 +25,16 @@ def playGame():
         else:
             print("請輸入提示範圍內的數字")
     print(f"您猜了{count}次")
+
+class Student():
+    def __init__(self,name:str,chinese:int,english:int,math:int): #自訂的初始化
+        self.name = name
+        self.chinese = chinese
+        self.english = english
+        self.math = math
+
+    def sum(self)->int: #實體方法
+        return self.chinese + self.english + self.math
+    
+def getStudent(name:str,chinese:int,math:int,) -> Student:
+    return Student(name=name,chinese=chinese,english=english,math=math)
